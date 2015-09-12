cb3c5225819ceeaa1c37b52a0e2c896aed3d7b43
Gil Barbara <gilbarbara@gmail.com>
2015-09-11 21:57:41 -0300

    add gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp        = require('gulp'),
     $           = require('gulp-load-plugins')(),
     del         = require('del'),
+    exec        = require('child_process').exec,
     merge       = require('merge-stream'),
-    runSequence = require('run-sequence');
+    runSequence = require('run-sequence'),
+    vinylPaths  = require('vinyl-paths');
 
 // Script
 gulp.task('lint', function () {
@@ -37,6 +39,49 @@ gulp.task('scripts', ['lint'], function () {
 
 gulp.task('clean', function (cb) {
     return del(['dist/*'], cb);
+});
+
+gulp.task('get-commit', function (cb) {
+    exec('git log -1 --pretty=%s && git log -1 --pretty=%b', function (err, stdout, stderr) {
+        var parts = stdout.replace('\n\n', '').split('\n');
+
+        commitMessage = parts[0];
+        if (parts[1]) {
+            commitMessage += ' — ' + parts[1];
+        }
+
+        cb(err);
+    });
+});
+
+gulp.task('gh-pages', function () {
+
+    var clean,
+        push;
+
+    clean = gulp.src('.publish/.DS_Store')
+        .pipe(vinylPaths(del));
+
+    push = gulp.src([
+            'index.html',
+            'dist/**/*'
+        ], { base: './' })
+        .pipe($.ghPages({
+            branch: 'gh-pages',
+            message: commitMessage,
+            force: true,
+            push: false
+        }));
+
+    return merge(clean, push);
+});
+
+gulp.task('deploy-pages', function (cb) {
+    runSequence(['build', 'get-commit'], 'gh-pages', cb);
+});
+
+gulp.task('serve', function () {
+    gulp.watch('index.js', ['build']);
 });
 
 gulp.task('build', function (cb) {
