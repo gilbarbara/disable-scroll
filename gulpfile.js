3a2f5326e74cd0df082a711172e41df259426cbf
Gil Barbara <gilbarbara@gmail.com>
2016-10-07 13:14:09 -0300

    v0.3.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,92 @@
-var gulp        = require('gulp'),
-    $           = require('gulp-load-plugins')(),
-    del         = require('del'),
-    exec        = require('child_process').exec,
-    merge       = require('merge-stream'),
-    runSequence = require('run-sequence'),
-    vinylPaths  = require('vinyl-paths');
+var gulp = require('gulp'),
+  $ = require('gulp-load-plugins')(),
+  del = require('del'),
+  exec = require('child_process').exec,
+  merge = require('merge-stream'),
+  runSequence = require('run-sequence'),
+  vinylPaths = require('vinyl-paths');
+
+var commitMessage = '';
 
 // Script
-gulp.task('lint', function () {
-    return gulp.src('lib/scripts/*')
-        .pipe($.eslint({
-            useEslintrc: true,
-            rules: {
-                'no-console': 2
-            }
-        }))
-        .pipe($.eslint.format())
-        .pipe($.eslint.failOnError());
+gulp.task('lint', function() {
+  return gulp.src('lib/scripts/*')
+    .pipe($.eslint({
+      rules: {
+        'no-console': 2
+      }
+    }))
+    .pipe($.eslint.format())
+    .pipe($.eslint.failOnError());
 });
 
-gulp.task('scripts', ['lint'], function () {
-    return gulp.src(['index.js'])
-        .pipe($.umd({
-            exports: function () {
-                return 'disableScroll';
-            },
-            namespace: function () {
-                return 'disableScroll';
-            }
-        }))
-        .pipe($.replace('module.exports = disableScroll;\n\n', ''))
-        .pipe($.rename('disable-scroll.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe($.uglify())
-        .pipe($.rename('disable-scroll.min.js'))
-        .pipe(gulp.dest('dist'));
+gulp.task('scripts', ['lint'], function() {
+  return gulp.src(['index.js'])
+    .pipe($.umd({
+      exports: function() {
+        return 'disableScroll';
+      },
+      namespace: function() {
+        return 'disableScroll';
+      }
+    }))
+    .pipe($.replace('module.exports = disableScroll;\n\n', ''))
+    .pipe($.rename('disable-scroll.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe($.uglify())
+    .pipe($.rename('disable-scroll.min.js'))
+    .pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', function (cb) {
-    return del(['dist/*'], cb);
+gulp.task('clean', function(cb) {
+  return del(['dist/*'], cb);
 });
 
-gulp.task('get-commit', function (cb) {
-    exec('git log -1 --pretty=%s && git log -1 --pretty=%b', function (err, stdout, stderr) {
-        var parts = stdout.replace('\n\n', '').split('\n');
+gulp.task('get-commit', function(cb) {
+  exec('git log -1 --pretty=%s && git log -1 --pretty=%b', function(err, stdout) {
+    var parts = stdout.replace('\n\n', '').split('\n');
 
-        commitMessage = parts[0];
-        if (parts[1]) {
-            commitMessage += ' — ' + parts[1];
-        }
+    commitMessage = parts[0];
+    if (parts[1]) {
+      commitMessage += ' — ' + parts[1];
+    }
 
-        cb(err);
-    });
+    cb(err);
+  });
 });
 
-gulp.task('gh-pages', function () {
+gulp.task('gh-pages', function() {
 
-    var clean,
-        push;
+  var clean,
+    push;
 
-    clean = gulp.src('.publish/.DS_Store')
-        .pipe(vinylPaths(del));
+  clean = gulp.src('.publish/.DS_Store')
+    .pipe(vinylPaths(del));
 
-    push = gulp.src([
-            'index.html',
-            'dist/**/*'
-        ], { base: './' })
-        .pipe($.ghPages({
-            branch: 'gh-pages',
-            message: commitMessage,
-            force: true
-        }));
+  push = gulp.src([
+    'index.html',
+    'dist/**/*'
+  ], { base: './' })
+    .pipe($.ghPages({
+      branch: 'gh-pages',
+      message: commitMessage,
+      force: true
+    }));
 
-    return merge(clean, push);
+  return merge(clean, push);
 });
 
-gulp.task('deploy-pages', function (cb) {
-    runSequence(['build', 'get-commit'], 'gh-pages', cb);
+gulp.task('deploy-pages', function(cb) {
+  runSequence(['build', 'get-commit'], 'gh-pages', cb);
 });
 
-gulp.task('serve', function () {
-    gulp.watch('index.js', ['build']);
+gulp.task('serve', function() {
+  gulp.watch('index.js', ['build']);
 });
 
-gulp.task('build', function (cb) {
-    process.env.NODE_ENV = 'production';
-    runSequence('clean', 'scripts', cb);
+gulp.task('build', function(cb) {
+  process.env.NODE_ENV = 'production';
+  runSequence('clean', 'scripts', cb);
 });
 
 gulp.task('default', ['build']);
