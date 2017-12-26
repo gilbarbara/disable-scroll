b1c72499215f94be2caa2a486ba40cc66bd1ed70
Gil Barbara <gilbarbara@gmail.com>
2017-12-26 19:55:20 -0200

    rename options properties
    disableScrollbar -> disableScroll
    scrollEventKeys -> keyboardKeys

--- src/index.js
+++ src/index.js
@@ -12,10 +12,10 @@ class DisableScroll {
 
     this.options = {
       disableWheel: true,
-      disableScrollbar: true,
+      disableScroll: true,
       disableKeys: true,
-      scrollEventKeys: [32, 33, 34, 35, 36, 37, 38, 39, 40]
-      // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+      keyboardKeys: [32, 33, 34, 35, 36, 37, 38, 39, 40]
+      // space: 32, page up: 33, page down: 34, end: 35, home: 36
       // left: 37, up: 38, right: 39, down: 40
     };
 
@@ -39,19 +39,19 @@ class DisableScroll {
       ...options,
     };
 
-    const { disableKeys, disableScrollbar, disableWheel } = this.options;
+    const { disableKeys, disableScroll, disableWheel } = this.options;
 
     if (disableWheel) {
       document.addEventListener('wheel', this.handleWheel);
       document.addEventListener('touchmove', this.handleWheel);
     }
 
-    if (disableScrollbar) {
+    if (disableScroll) {
       this.lockToScrollPos = [
         this.element.scrollX,
         this.element.scrollY
       ];
-      document.addEventListener('scroll', this.handleScrollbar);
+      document.addEventListener('scroll', this.handleScroll);
     }
 
     if (disableKeys) {
@@ -67,7 +67,7 @@ class DisableScroll {
 
     document.removeEventListener('wheel', this.handleWheel);
     document.removeEventListener('touchmove', this.handleWheel);
-    document.removeEventListener('scroll', this.handleScrollbar);
+    document.removeEventListener('scroll', this.handleScroll);
     document.removeEventListener('keydown', this.handleKeydown);
   }
 
@@ -75,19 +75,19 @@ class DisableScroll {
     e.preventDefault();
   };
 
-  handleScrollbar = () => {
-    window.scrollTo(this.lockToScrollPos[0], this.lockToScrollPos[1]);
+  handleScroll = () => {
+    window.scrollTo(...this.lockToScrollPos);
   };
 
   handleKeydown = (e) => {
-    for (let i = 0; i < this.options.scrollEventKeys.length; i++) {
-      if (
-        e.keyCode === this.options.scrollEventKeys[i]
-        && !['INPUT', 'TEXTAREA'].includes(e.target.tagName)
-      ) {
-        e.preventDefault();
-        return;
-      }
+    let keys = [...this.options.keyboardKeys];
+
+    if (['INPUT', 'TEXTAREA'].includes(e.target.tagName)) {
+      keys = keys.slice(1);
+    }
+
+    if (keys.includes(e.keyCode)) {
+      e.preventDefault();
     }
   }
 }
